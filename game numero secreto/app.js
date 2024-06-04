5df5ec17eca65c4502ab911bc4a90f8c16167bf9
Yasmin Costa <105598507+YasminAssisCosta@users.noreply.github.com>
2024-06-04 16:57:04 -0300

    3

--- game numero secreto/app.js
+++ game numero secreto/app.js
@@ -1,5 +1,12 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre um e 10';
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+function verificarChute(){
+    alert('ola')
+};
